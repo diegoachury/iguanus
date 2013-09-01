6b162355eb136a7de2a7a45d8601dcb8011261de
diegoachury <tiran_o@hotmail.com>
2013-09-01 10:27:31 -0500

    iniciando repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iguanus::Application.config.secret_token = '76d788d5e76ea83c8e73cd40b33bd698ac6a9ae8929d4b7e472d7d413eea3b783e3d02d886df19f974d801e22cde667d1d173cd5822f4ed296bd850242291fb2'
